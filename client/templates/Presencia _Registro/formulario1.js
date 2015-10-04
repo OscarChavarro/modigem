409515296be26e731a0e4a81ae2785b0dea1a412
SithMaster <jedilink@gmail.com>
2015-10-04 15:09:46 -0500

    0h01min: Cambio de formato

--- client/templates/Presencia _Registro/formulario1.js
+++ client/templates/Presencia _Registro/formulario1.js
@@ -1,17 +1,17 @@
 Template.formulario1.events({ 
-  'submit form': function(e) {
-	   e.preventDefault();
-    
-	var NuevoUsuario= {
-	 museo: $(e.target).find('[name=datos1]').val(),
-   	 nombre: $(e.target).find('[name=datos2]').val(),
-	 apellido: $(e.target).find('[name=datos3]').val(),
-	 usuario: $(e.target).find('[name=datos4]').val(),
-	 correo: $(e.target).find('[name=datos5]').val(),
-	 contraseña: $(e.target).find('[name=datos6]').val(),
-	 
-	 
-	};
-	userdata.insert(NuevoUsuario);
+    "submit form": function(e) {
+	    e.preventDefault();
+
+		console.log("Agregando registro");
+		
+	    var NuevoUsuario= {
+			museo: $(e.target).find('[name=datos1]').val(),
+			nombre: $(e.target).find('[name=datos2]').val(),
+			apellido: $(e.target).find('[name=datos3]').val(),
+			usuario: $(e.target).find('[name=datos4]').val(),
+			correo: $(e.target).find('[name=datos5]').val(),
+			contraseña: $(e.target).find('[name=datos6]').val()	 
+	    };
+	    userdata.insert(NuevoUsuario);
 	}
-	});
+});
