ea10f0c181dd487b2bf2ade65c13b1425f802f6c
root <root@modigem.museodigital.org>
2015-10-08 12:48:49 +0000

    0h01min: mensajes en consola

--- client/templates/cuentasUsuario/logInForm.js
+++ client/templates/cuentasUsuario/logInForm.js
@@ -2,8 +2,10 @@
   'submit form': function(e) { 
     e.preventDefault();
     logemail = $(e.target).find('[name=email]').val(); 	
-	logpassword = $(e.target).find('[name=password]').val();
-	Meteor.loginWithPassword(logemail, logpassword,);
+    logpassword = $(e.target).find('[name=password]').val();
+    console.log("Entrando como usuario " + logemail);
+    console.log("Con la clave " + logpassword);
+    Meteor.loginWithPassword(logemail, logpassword);
     setTimeout(function(){
     if (Meteor.user()) {
 			   if (adminList.find({newAdmin: Meteor.user().username}).count() + 1 > 1){
