c1b407e3b4c99fb7dc5f0198982e10c4e9b058a1
EduardoRamirez <eduardo.ram.1992@gmail.com>
2015-11-02 19:47:45 -0430

    Buscando errores

--- client/templates/manageUsers.js
+++ client/templates/manageUsers.js
@@ -1,1 +1,14 @@
-Router.route("manageUsers", {name: "manageUsers"});
+
+Router.route("manageUsers", {name: "manageUsers",
+    data: function() {
+    	console.log("En cada ruta hay una funcion que se llama siempre al inicio y se llama data");
+    	Meteor.subscribe("allUsers");
+    }
+});
+
+Template.manageUsers.helpers({
+	usersList: function () {
+		console.log("aca estoy ****");
+		return users.find();
+	}
+});
