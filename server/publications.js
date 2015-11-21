c279f0654a164a7eb58cbfccd08420c886415e21
Rogerio J. Sánchez <rs7.contractors@gmail.com>
2015-11-21 19:17:18 -0430

    3h00min: Estudiar las propiedades del paquete accounts-ui y diseñar lógica de autentificación de usuarios vs roles Nota: En la publicación de la colección Meteor.users() se presentan problemas a hacer: Meteor.users.find() del lado del cliente (solo se muestra al current user en el objeto que debe contener todos los usuarios.

--- server/publications.js
+++ server/publications.js
@@ -18,9 +18,9 @@ Meteor.publish('allLandingTexts', function(){
 Meteor.publish('allAdmins', function(){
 	return adminList.find();
 });
-//Meteor.publish('allUsers', function(){
-//	return users.find();
-//});
+Meteor.publish('allUsers', function(){
+	return Meteor.users.find();
+});
 Meteor.publish('allUserRole', function(){
 	return userRole.find();
 });
