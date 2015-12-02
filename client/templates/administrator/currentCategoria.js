9ec02406e6ace773f47b9a2a64326ca109681836
Heberto Romay <heberomay16@gmail.com>
2015-12-01 20:55:23 -0430

    Completado: ipc02, ipc03, ipc05,  ipc06, ipc07, ipc08, ipc09, ipc10, ipc11, ipc12, ipc13

--- client/templates/administrator/currentCategoria.js
+++ client/templates/administrator/currentCategoria.js
@@ -1,15 +1,30 @@
 Template.currentCategoria.helpers ({
-    runner: function() {
-		variable = piece.find().fetch()[piece.find().count()-1].categoria;
-		    if (variable="Visual"){
-			    return true;
-			    
-		    } else if (variable="Texto"){
-				return true;
-			} else if (variable="AudioVisual"){
-				return true;
-			} else if (variable="Auditivo"){
-				return true;
+    VISUAL: function() {
+		    if (piece.find().fetch()[piece.find().count()-1].categoria=='Visual'){
+				return true;		    
+		    }else{
+				return false
+			}
+	    },
+    TEXTO: function() {
+		    if (piece.find().fetch()[piece.find().count()-1].categoria=='Texto'){
+				return true;		    
+		    }else{
+				return false
+			}
+	    },
+    AUDIOVISUAL: function() {
+		    if (piece.find().fetch()[piece.find().count()-1].categoria=='AudioVisual'){
+				return true;		    
+		    }else{
+				return false
+			}
+	    },
+	AUDITIVO: function() {
+		    if (piece.find().fetch()[piece.find().count()-1].categoria=='Auditivo'){
+				return true;		    
+		    }else{
+				return false
 			}
 	    },
 	piece: function() {
