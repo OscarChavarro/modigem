8678821aa38066a7ea2bab896d0e4f99eb41b0af
Rogerio Sanchez <rs7.contractors@gmail.com>
2015-12-03 17:51:18 -0430

    0h:30min Ajustando la configuración de facebook-config.js

--- server/facebook-config.js
+++ server/facebook-config.js
@@ -2,8 +2,17 @@
 ServiceConfiguration.configurations.remove({
     service: 'facebook'
 });
+if (process.env.HOSTNAME === 'modigem.museodigital.org') {
  ServiceConfiguration.configurations.insert({
     service: 'facebook',
     appId: '539374709554821',
     secret: 'fa62dfade8567db49830fcd8ae6cb471'
 });
+        }else{
+ ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1671219196483781',
+    secret: 'f864e2c59979eaed7c47b3905d1403eb'
+});
+}
+
