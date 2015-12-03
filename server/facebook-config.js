c267b5cb329c2618c04df6398ee234ddd5d51f41
Rogerio Sanchez <rs7.contractors@gmail.com>
2015-12-03 18:22:47 -0430

    0h5m agregando comentarios

--- server/facebook-config.js
+++ server/facebook-config.js
@@ -5,13 +5,13 @@ ServiceConfiguration.configurations.remove({
 if (process.env.HOSTNAME === 'modigem.museodigital.org') {
  ServiceConfiguration.configurations.insert({
     service: 'facebook',
-    appId: '539374709554821',
+    appId: '539374709554821', //AppId con URL registrada: 'http://modigem.museodigital.org/'
     secret: 'fa62dfade8567db49830fcd8ae6cb471'
 });
         }else{
  ServiceConfiguration.configurations.insert({
     service: 'facebook',
-    appId: '1671219196483781',
+    appId: '1671219196483781', //AppId con URL registrada: 'http://localhost:3000'
     secret: 'f864e2c59979eaed7c47b3905d1403eb'
 });
 }
