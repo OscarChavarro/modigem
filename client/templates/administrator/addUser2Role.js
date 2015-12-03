5b92956f03269915ce13907a438a56bc8d6f90d8
Rogerio Sanchez <rs7.contractors@gmail.com>
2015-12-03 19:09:40 -0430

    0h:04m Se corrige error de sintaxis

--- client/templates/administrator/addUser2Role.js
+++ client/templates/administrator/addUser2Role.js
@@ -3,15 +3,15 @@ Template.addUser2Role.events({
         e.preventDefault();
         userId2Up = $(e.target).find('[name=name]').val();
         rolUser     = $(e.target).find('[name=rol]').val();
-		if(user2Role.find({user: Meteor.users.findOne({_id: userId2Up})}).count()+1>1){
+		if(user2Role.find({user: Meteor.users.findOne({_id: userId2Up})})._id.count()+1>1){
 	    //Remover al usuario del rol anterior	
 		}
 	    //else{ ASIGNARLE NUEVO ROL
         if (rolUser==1){
-            user2Role.insert({rol: userRole.findOne({nameC: 'ADMINISTRATOR'})._id, user: Meteor.users.findOne({_id: userId2Up})});
+            user2Role.insert({rol: userRole.findOne({nameC: 'ADMINISTRATOR'})._id, user: Meteor.users.findOne({_id: userId2Up})._id});
         }
         if (rolUser==2){
-            user2Role.insert({rol: userRole.findOne({nameC: 'RESTORER'})._id, user: Meteor.users.findOne({_id: userId2Up})});
+            user2Role.insert({rol: userRole.findOne({nameC: 'RESTORER'})._id, user: Meteor.users.findOne({_id: userId2Up})._id});
         }
 		//}
         return false;
