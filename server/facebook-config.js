36947c991c001fc76fb4ee49c3b8850b55bfb54b
Rogerio Sanchez <rs7.contractors@gmail.com>
2015-12-03 16:34:15 -0430

    0h15min Nuevo appId de facebook + nuevo secret todo bajo el dominio de modigem.museodigital.org

--- server/facebook-config.js
+++ server/facebook-config.js
@@ -4,6 +4,6 @@ ServiceConfiguration.configurations.remove({
 });
  ServiceConfiguration.configurations.insert({
     service: 'facebook',
-    appId: '442216965971471',
-    secret: 'a37253a3fa72d933a2c96ace746119f4'
+    appId: '539374709554821',
+    secret: 'fa62dfade8567db49830fcd8ae6cb471'
 });
