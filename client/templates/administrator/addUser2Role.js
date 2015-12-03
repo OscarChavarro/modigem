e52013c20fb4e6472601bb5d831de8943483372f
Rogerio Sanchez <rs7.contractors@gmail.com>
2015-12-03 18:52:37 -0430

    0h:05m se agregan roles de usuario por _id

--- client/templates/administrator/addUser2Role.js
+++ client/templates/administrator/addUser2Role.js
@@ -1,17 +1,17 @@
 Template.addUser2Role.events({ 
     'submit form': function(e) {
         e.preventDefault();
-        username2Up = $(e.target).find('[name=name]').val();
+        userId2Up = $(e.target).find('[name=name]').val();
         rolUser     = $(e.target).find('[name=rol]').val();
-		if(user2Role.find({user: Meteor.users.findOne({username: username2Up})._id}).count()+1>1){
+		if(user2Role.find({user: Meteor.users.findOne({_id: userId2Up})}).count()+1>1){
 	    //Remover al usuario del rol anterior	
 		}
 	    //else{ ASIGNARLE NUEVO ROL
         if (rolUser==1){
-            user2Role.insert({rol: userRole.findOne({nameC: 'ADMINISTRATOR'})._id, user: Meteor.users.findOne({username: username2Up})._id});
+            user2Role.insert({rol: userRole.findOne({nameC: 'ADMINISTRATOR'})._id, user: Meteor.users.findOne({_id: userId2Up})});
         }
         if (rolUser==2){
-            user2Role.insert({rol: userRole.findOne({nameC: 'RESTORER'})._id, user: Meteor.users.findOne({username: username2Up})._id});
+            user2Role.insert({rol: userRole.findOne({nameC: 'RESTORER'})._id, user: Meteor.users.findOne({_id: userId2Up})});
         }
 		//}
         return false;
