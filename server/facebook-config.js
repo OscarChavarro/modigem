63f9fc039ef59b4e65226766cb8b94a6c673c979
Rogerio Sanchez <rs7.contractors@gmail.com>
2015-12-03 20:26:41 -0430

    Se devuelve a la configuración de una sola appId

--- server/facebook-config.js
+++ server/facebook-config.js
@@ -2,17 +2,10 @@
 ServiceConfiguration.configurations.remove({
     service: 'facebook'
 });
-if (process.env.HOSTNAME === 'modigem.museodigital.org') {
+
  ServiceConfiguration.configurations.insert({
     service: 'facebook',
     appId: '539374709554821', //AppId con URL registrada: 'http://modigem.museodigital.org/'
     secret: 'fa62dfade8567db49830fcd8ae6cb471'
 });
-        }else{
- ServiceConfiguration.configurations.insert({
-    service: 'facebook',
-    appId: '1671219196483781', //AppId con URL registrada: 'http://localhost:3000'
-    secret: 'f864e2c59979eaed7c47b3905d1403eb'
-});
-}
 
