3b535fc6a66168a05a3407f783375d0e39da9d1f
Rogerio Sanchez <rs7.contractors@gmail.com>
2015-12-07 13:37:13 -0430

    Restart google services

--- server/googleConfig.js
+++ server/googleConfig.js
@@ -1,9 +1,9 @@
-GOOGLE
-Accounts.loginServiceConfiguration.remove({
-  service: 'google'
-});
-Accounts.loginServiceConfiguration.insert({
-  service: 'google',
-  clientId: 'modigem-1151',
-  secret: 'AIzaSyAmpSDpXGJjMrGy83lfTJSgmcsT0YfiqVo'
-});
+//GOOGLE
+//Accounts.loginServiceConfiguration.remove({
+//  service: 'google'
+//});
+//Accounts.loginServiceConfiguration.insert({
+//  service: 'google',
+//  clientId: 'modigem-1151',
+//  secret: 'AIzaSyAmpSDpXGJjMrGy83lfTJSgmcsT0YfiqVo'
+//});
