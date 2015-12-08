ae221e52ef77e9b0300de3c56eeee65c4fc750b8
Rogerio Sanchez <rs7.contractors@gmail.com>
2015-12-08 00:45:41 -0430

    1h00m configurando G+

--- server/googleConfig.js
+++ server/googleConfig.js
@@ -1,9 +1,17 @@
 //GOOGLE
-//Accounts.loginServiceConfiguration.remove({
-//  service: 'google'
-//});
-//Accounts.loginServiceConfiguration.insert({
-//  service: 'google',
-//  clientId: 'modigem-1151',
-//  secret: 'AIzaSyAmpSDpXGJjMrGy83lfTJSgmcsT0YfiqVo'
-//});
+ServiceConfiguration.configurations.remove({
+    service: 'google'
+});
+if (process.env.HOSTNAME === 'modigem.museodigital.org') {
+ ServiceConfiguration.configurations.insert({
+    service: 'google',
+    clientId: '867822180775-hevb99lqr3fhh1q85g3e7ec5b19hslae.apps.googleusercontent.com', //clientId con URL registrada: 'http://modigem.museodigital.org/' MODIGEM
+    secret: 'vGDl7b1yWCuDY453rFUSFc61'
+});
+        }else{
+ ServiceConfiguration.configurations.insert({
+    service: 'google',
+    consumerKey: '17624622797-be1qj7233o12i4glmsbqkvk47p06krof.apps.googleusercontent.com', //clientId con URL registrada: 'http://localhost:3000'
+    secret: 'HHuPTT9KU6wde-Wp-mSo496I'
+});
+}
