ddec58ab0f93594a490ee288d2b801abf91e5c1e
SithMaster <jedilink@gmail.com>
2015-12-07 23:13:15 -0500

    1h00min: Cambios en el listado de usuarios

--- client/templates/content/administrators/usersTable.js
+++ client/templates/content/administrators/usersTable.js
@@ -1,9 +1,28 @@
+Template.checkboxForUserRole.helpers({
+    checkIfActive: function(u, r)
+    {
+        var filter = {userId: u, userRoleId: r};
+        var check = user2role.findOne(filter);
+
+        if ( valid(check) ) {
+            return "checked";
+        }
+        return "unchecked";
+    }
+});
+
 Template.usersTable.helpers({
     USER: function() {
         return Meteor.users.find(); //Solo disponible para el admin
                                            
     },
+	getArrayOfUserProfiles: function()
+	{
+		return userRole.find().fetch();
+	},
 	/**
+	Calcula el nombre del usuario a partir de un elemento de la
+	tabla users en el paquete Meteor.Accounts.
 	this es un user
 	*/
 	showUserName: function()
@@ -15,6 +34,25 @@ Template.usersTable.helpers({
 			return this.profile.name;
 		}
 		return "No tiene un nombre válido";
+	},
+	/**
+	Calcula el correo del usuario a partir de un elemento de la
+	tabla users en el paquete Meteor.Accounts.
+	this es un user
+	*/
+	showUserEmail: function()
+	{
+		if ( valid(this.emails) &&
+  		     valid(this.emails[0]) && 
+			 valid(this.emails[0].address) ) {
+			return this.emails[0].address;
+		}
+		else if ( valid(this.services) &&
+		          valid(this.services.facebook) &&
+				  valid(this.services.facebook.email) ) {
+			return this.services.facebook.email + " (facebook)";
+		}
+		return "No tiene un email válido";
 	}
 });
 
