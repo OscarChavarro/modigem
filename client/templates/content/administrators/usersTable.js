52028c8263aab264969115ed7fa3a59801d6ac1e
SithMaster <jedilink@gmail.com>
2015-12-07 21:24:33 -0500

    0h26min: Modificado módulo de gestión de usuarios para mostrar y manejar de una mejor manera la lista de usuarios. Incluye código que logra manejar las diferentes maneras de almacenar el nombre de usuario en la colección "users" del paquete Accounts de Meteor. :)

--- client/templates/content/administrators/usersTable.js
+++ client/templates/content/administrators/usersTable.js
@@ -2,5 +2,36 @@ Template.usersTable.helpers({
     USER: function() {
         return Meteor.users.find(); //Solo disponible para el admin
                                            
-    }
+    },
+	/**
+	this es un user
+	*/
+	showUserName: function()
+	{
+		if ( valid(this.username) ) {
+			return this.username;
+		}
+		else if ( valid(this.profile.name) ) {
+			return this.profile.name;
+		}
+		return "No tiene un nombre válido";
+	}
+});
+
+Template.usersTable.events({
+	/**
+	En la tabla que lista los usuarios dentro del panel
+	de aministración de usuario, se muestra un botón
+	"¡Eliminar!" para cada usuario. Cuando el usuario 
+	oprime uno de esos botones se llama esta función.
+	Nótese que cada botón contiene un identificador
+	que contiene el _id de la base de datos del usuario
+	a eliminar.
+	*/
+    "submit #deleteUser": function(event, template)
+	{
+		event.preventDefault();
+		var id = event.target.delete.id;
+		Meteor.users.remove(id);
+	}	
 });
