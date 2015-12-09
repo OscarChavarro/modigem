07f85fd69e35184b23478fa628b4f39d2f4948c0
Heberto Romay <heberomay16@gmail.com>
2015-12-08 20:05:13 -0430

    Form edit test

--- client/templates/administrator/pieceForm.js
+++ client/templates/administrator/pieceForm.js
@@ -1,6 +1,16 @@
+Template.toFormularioPieza.helpers({
+	allForms:function(){
+		return serviceRequestForm.find();
+	}
+});
+
 Template.toFormularioPieza.events({  
-    'click button': function() {
-        Router.go('formularioPieza1');
-        return;
+    "click #formEditId": function(event, template) {
+        var id = event.currentTarget.getAttribute("data-id");
+        if ( valid(id) ) {
+            console.log("[formularioPieza1]: Seleccionando formulario con id " + 
+                id + " para edición");
+            Session.set("formEditId", id);
+        }
     }
 });
--- client/templates/administrator/pieceForm1.js
+++ client/templates/administrator/pieceForm1.js
@@ -1,88 +1,132 @@
-Template.formularioPieza1.onCreated ( function (){
-    this.Visual= new ReactiveVar(false);
-    this.Texto= new ReactiveVar(false);
-    this.AudioVisual= new ReactiveVar(false);
-    this.Auditivo= new ReactiveVar(false);
-});
-
 Template.formularioPieza1.helpers({
-    Visual: function() {
-        return Template.instance().Visual.get();
+    /**
+    */
+    buildAnswerOptions: function(question) {
+        var varname = "EditAnswerOptionsArrayFor" + question._id;
+        var arr;
+        arr = Session.get(varname);
+        return buildAnswerPartFromCType(question, arr);
     },
-    Texto: function() {
-        return Template.instance().Texto.get();
-    },
-    AudioVisual: function(){
-        return Template.instance().AudioVisual.get();
-    },
-    Auditivo: function() {
-        return Template.instance().Auditivo.get();
-    }
-});
-Template.formularioPieza1.events({
-    'click label': function( event, template ) {
-        if ( $( event.target ).val() === "Visual" ) {
-            template.Visual.set( true );
-            template.Texto.set(false);
-            template.AudioVisual.set(false);
-            template.Auditivo.set(false);    
-        } else if ( $( event.target ).val() === "Texto" ){
-            template.Visual.set(false);
-            template.Texto.set(true);
-            template.AudioVisual.set(false);
-            template.Auditivo.set(false);
-        } else if ( $( event.target ).val() === "AudioVisual" ){
-            template.Visual.set(false);
-            template.Texto.set(false);
-            template.AudioVisual.set(true);
-            template.Auditivo.set(false);
-        } else if ($( event.target ).val() === "Auditivo") {
-            template.Visual.set(false);
-            template.Texto.set(false);
-            template.AudioVisual.set(false);
-            template.Auditivo.set(true);
-        } else {
-            template.Visual.set(false);
-            template.Texto.set(false);
-            template.AudioVisual.set(false);
-            template.Auditivo.set(false);
-        }
-  },
-    'submit form': function(e) {
-        e.preventDefault();
-        var llorar= document.getElementsByName("medio");
-            for(var i=0; i<llorar.length; i++) {
-                if (llorar[i].checked==true) {
-                inputTipo = llorar[i].value;
+    /**
+    Realiza un llamado al procedimiento remoto "getQueriesFromFormId" en el
+    lado del servidor y hace disponible la información de las preguntas como
+    una variable de sesión que constituye un caché para la sesión de usuario
+    actual.
+    */
+    startForm: function(fid) {
+        // Borra el caché, ya que en modo edición la información cambia
+        Session.set("EditQueryArrayFor" + fid, null);
+
+        Meteor.call("getQueriesFromFormId", fid, 
+            function(err, response) {
+                if ( valid(err) ) {
+                    console.log("  - Mensaje de error: " + err);
+                }
+                else if ( valid(response) && 
+                     valid(response.id) && valid(response.array) ) {
+                    Session.set("EditQueryArrayFor" + response.id, response.array);
                 }
             }
-        var NuevaPieza = {
-            nombre1: $(e.target).find('[name=nombrepieza]').val(),
-            nombre2: $(e.target).find('[name=nombrepopular]').val(),
-            etiqueta: $(e.target).find('[name=etiqueta]').val(),
-            descripcion: $(e.target).find('[name=descripcion]').val(),
-            categoria: inputTipo,
-            obra: $(e.target).find('[name=obra]').val(),
-            tecnica: $(e.target).find('[name=tecnica]').val(),
-            dimensiones: $(e.target).find('[name=dimensiones]').val(),
-            caracteres: $(e.target).find('[name=caracteres]').val(),
-            idioma: $(e.target).find('[name=idioma]').val(),
-            tiempo: $(e.target).find('[name=tiempo]').val(),
-            formato: $(e.target).find('[name=formato]').val(),
-            duracion: $(e.target).find('[name=duracion]').val(),
-            streaming: $(e.target).find('[name=streaming]').val(),
-            inicio: $(e.target).find('[name=inicio]').val(),
-            fin: $(e.target).find('[name=fin]').val(),
-            origen: $(e.target).find('[name=origen]').val(),
-            ubicacion: $(e.target).find('[name=ubicacion]').val(),
-            coleccion: $(e.target).find('[name=coleccion]').val(),
-            estado: $(e.target).find('[name=estado]').val(),
-            imagen: $(e.target).find('[name=imagen]').val(),
-            imagenID: images.find().fetch()[images.find().count()-1]._id
-        };
-        piece.insert(NuevaPieza);
-        
-        
+        );
+
+        return "";
+    },
+    /**
+    Esta función implementa una barrera de bloqueo para la sincronización de
+    la plantilla con la disponibilidad de la variable de sesión para el caché
+    de las preguntas en el formulario fid.
+    */
+    questionListReady: function(fid) {
+        if ( !valid(fid) ) {
+            return false;
+        }
+        var v = Session.get("EditQueryArrayFor" + fid);
+
+        if ( !valid(v) ) {
+            return false;
+        }
+        return true;
+    },
+    /**
+    Versión optimizada para pasar la colección a la plantilla en Spacebars.
+    Nótese que retorna el caché en memoria para la variable de sesión
+    correspondiente al formulario identificado con fid. Dado que la
+    operación Session.get es reactiva, la información será mostrada
+    tan pronto como esta variable esté disponible.
+    */
+    dbServiceRequestForm2QueryFiltered: function(fid) {
+        var errorArray = [
+            {"nameSpa" : "Error conectándose a la base de datos"},
+            {"nameSpa" : "ver módulo serviceRequestForm.js"}
+        ];
+
+        var array = Session.get("EditQueryArrayFor" + fid);
+
+        if ( valid(array) ) {
+            return array;
+        }
+        return errorArray;
+    },
+    /**
+    Realiza un llamado al procedimiento remoto "getAnswerOptionsFromQueryId" en el
+    lado del servidor y hace disponible la información de las opciones de respuesta en
+    una variable de sesión que constituye un caché para la sesión de usuario
+    actual.
+    */
+    startQuery: function(qid) {
+        Meteor.call("getAnswerOptionsFromQueryId", qid, 
+            function(err, response) {
+                if ( valid(err) ) {
+                    console.log("  - Mensaje de error: " + err);
+                }
+                else if ( valid(response) && 
+                     valid(response.id) && valid(response.array) ) {
+                    var varname = "EditAnswerOptionsArrayFor" + response.id;
+                    Session.set(varname, response.array);
+                }
+            }
+        );
+
+        return "";
+    },
+    /**
+    Esta función implementa una barrera de bloqueo para la sincronización de
+    la plantilla con la disponibilidad de la variable de sesión para el caché
+    de las opciones de respuesta para la pregunta qid.
+    */
+    answerOptionsListReady: function(qid) {
+        var varname = "EditAnswerOptionsArrayFor" + qid;
+
+        if ( !valid(qid) ) {
+            return false;
+        }
+        var v = Session.get(varname);
+
+        if ( !valid(v) ) {
+            return false;
+        }
+
+        return true;
+    },
+    /**
+    Versión optimizada para pasar la colección a la plantilla en Spacebars.
+    Nótese que retorna el caché en memoria para la variable de sesión
+    correspondiente al formulario identificado con fid. Dado que la
+    operación Session.get es reactiva, la información será mostrada
+    tan pronto como esta variable esté disponible.
+    */
+    dbServiceRequestAnswerOptionFiltered: function(qid) {
+        console.log("Importando opciones de respuesta asociadas a la pregunta con ID: " + qid);
+        var errorArray = [
+            {"nameSpa" : "Error conectándose a la base de datos"},
+            {"nameSpa" : "ver módulo serviceRequestForm.js"}
+        ];
+        var varname = "EditAnswerOptionsArrayFor" + qid;
+        var array = Session.get(varname);
+
+        if ( valid(array) ) {
+            return array;
+        }
+        return errorArray;
     }
-    
 });
